87522c6097e8736f466563ca5d2d7a1e8cddcea0
peter nader <peternader6565@outlook.com>
2026-09-06 11:05:20 +0300

    Question 01

---
+++ Assignment 03/Assignment 03/Book.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Assignment_03
+{
+    internal class Book
+    {
+        public string Title;
+        public int Pages;
+
+        public override string ToString()
+        {
+            return $"{Title}, {Pages}";
+        }
+    }
+}
--- Assignment 03/Assignment 03/Program.cs
+++ Assignment 03/Assignment 03/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 01
+
+            //Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it.
+            Book book1 = new Book();
+            book1.Title = "My Book";
+            book1.Pages = 100;
+            object obj = book1;
+            Console.WriteLine(obj.ToString());
+            #endregion
         }
     }
 }
