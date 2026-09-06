dcd449e9cd5dbefbab09a52af1f094e579306d50
peter nader <peternader6565@outlook.com>
2026-09-06 11:37:00 +0300

    Question 03

--- Assignment 03/Assignment 03/Program.cs
+++ Assignment 03/Assignment 03/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Assignment_03
+﻿using System.Numerics;
+using System.Timers;
+
+namespace Assignment_03
 {
     internal class Program
     {
@@ -16,11 +19,19 @@
 
             #region Question 02
             //Using the Book class above, print the result of calling ToString(), Equals() (compare book with itself), GetHashCode(), and GetType() on book.
-            Book book1 = new Book();
-            Console.WriteLine(book1.ToString());
-            Console.WriteLine(book1.Equals(book1));
-            Console.WriteLine(book1.GetHashCode());
-            Console.WriteLine(book1.GetType());
+            //Book book1 = new Book();
+            //Console.WriteLine(book1.ToString());
+            //Console.WriteLine(book1.Equals(book1));
+            //Console.WriteLine(book1.GetHashCode());
+            //Console.WriteLine(book1.GetType());
+            #endregion
+
+            #region Question 03
+            //Look at the line below.Is it a compile - time error, a runtime error, or a logical error? Fix it.
+            //int pages = "464";
+            //Compile - time error — you can't put text (string) into an int variable.
+            //int pages = 464;
+
             #endregion
         }
     }
