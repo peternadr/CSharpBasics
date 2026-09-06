86f83d4b5aff1bd7afe43a9881659fd69ce54f05
peter nader <peternader6565@outlook.com>
2026-09-06 11:34:46 +0300

    Question 02

--- Assignment 03/Assignment 03/Book.cs
+++ Assignment 03/Assignment 03/Book.cs
@@ -6,9 +6,9 @@ namespace Assignment_03
         public string Title;
         public int Pages;
 
-        public override string ToString()
-        {
-            return $"{Title}, {Pages}";
-        }
+        //public override string ToString()
+        //{
+        //    return $"{Title}, {Pages}";
+        //}
     }
 }
--- Assignment 03/Assignment 03/Program.cs
+++ Assignment 03/Assignment 03/Program.cs
@@ -7,11 +7,20 @@
             #region Question 01
 
             //Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it.
+            //Book book1 = new Book();
+            //book1.Title = "My Book";
+            //book1.Pages = 100;
+            //object obj = book1;
+            //Console.WriteLine(obj);
+            #endregion
+
+            #region Question 02
+            //Using the Book class above, print the result of calling ToString(), Equals() (compare book with itself), GetHashCode(), and GetType() on book.
             Book book1 = new Book();
-            book1.Title = "My Book";
-            book1.Pages = 100;
-            object obj = book1;
-            Console.WriteLine(obj.ToString());
+            Console.WriteLine(book1.ToString());
+            Console.WriteLine(book1.Equals(book1));
+            Console.WriteLine(book1.GetHashCode());
+            Console.WriteLine(book1.GetType());
             #endregion
         }
     }
