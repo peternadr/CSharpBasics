6f83f0f872ca556c9d9b7804f542f71353b238d3
peter nader <peternader6565@outlook.com>
2026-09-09 11:37:37 +0300

    Qusstion 01

---
+++ Assignment 04/Assignment 04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace Assignment_04;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        #region Quesstion 01
+        /*Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. Print
+        both title and upperTitle to show that title did not change.*/
+
+        // Declare string title = "clean code";
+        string title = "clean code";
+
+        // Call title.ToUpper() and store it in a new variable upperTitle
+        string upperTitle = title.ToUpper();
+
+        // Print both title and upperTitle to show that title did not change
+        Console.WriteLine(title);
+        Console.WriteLine(upperTitle); 
+        #endregion
+    }
+}
