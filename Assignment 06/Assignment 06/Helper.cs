4755b3c82b25c3ce489e8119b5d9381e2993dbf7
peter nader <peternader6565@outlook.com>
2026-09-16 11:41:40 +0300

    Question 03

---
+++ Assignment 06/Assignment 06/Helper.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Assignment_06
+{
+    internal class Helper
+    {
+     
+        public static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Library!");
+        }
+
+    }
+}
--- Assignment 06/Assignment 06/Program.cs
+++ Assignment 06/Assignment 06/Program.cs
@@ -20,13 +20,21 @@
 shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.*/
 
             // Creat a two-dimentional array and assign it the values shelf 0 : {3,5}, shelf 1 : {1,4}
-            int[,] shelfCopies =
-            {
-                {3, 5 },
-                {1, 4 }
-            };
+            //int[,] shelfCopies =
+            //{
+            //    {3, 5 },
+            //    {1, 4 }
+            //};
 
-            Console.WriteLine(shelfCopies[1,0]); // 1
+            //Console.WriteLine(shelfCopies[1,0]); // 1
+
+            #endregion
+
+            #region Question 03
+            /*Write a method called PrintWelcomeMessage that takes no parameters and prints
+"Welcome to the Library!". Call it from Main.*/
+
+            Helper.PrintWelcomeMessage();
 
             #endregion
         }
