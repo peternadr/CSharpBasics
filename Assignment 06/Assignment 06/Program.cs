0f78e0f1ed75dcc0b1a0925a4f45491d754cbca3
peter nader <peternader6565@outlook.com>
2026-09-16 11:21:51 +0300

    Question 01

---
+++ Assignment 06/Assignment 06/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 01
+            /*Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print
+            the second price(index 1).*/
+
+            // Create a one-dimensional array and assign it values 25.5 , 40.0 , 33.75
+            double[] prices = { 25.5, 40.0, 33.75 };
+
+            Console.WriteLine(prices[1]); // 40
+            
+            #endregion
+        }
+    }
+}
