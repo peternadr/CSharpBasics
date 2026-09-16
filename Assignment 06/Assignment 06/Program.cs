fcb802b1f0ae8228927b152564c08038501dae45
peter nader <peternader6565@outlook.com>
2026-09-16 11:30:15 +0300

    Question 02

--- Assignment 06/Assignment 06/Program.cs
+++ Assignment 06/Assignment 06/Program.cs
@@ -9,10 +9,25 @@
             the second price(index 1).*/
 
             // Create a one-dimensional array and assign it values 25.5 , 40.0 , 33.75
-            double[] prices = { 25.5, 40.0, 33.75 };
+            //double[] prices = { 25.5, 40.0, 33.75 };
 
-            Console.WriteLine(prices[1]); // 40
-            
+            //Console.WriteLine(prices[1]); // 40
+
+            #endregion
+
+            #region Question 02
+            /*Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.*/
+
+            // Creat a two-dimentional array and assign it the values shelf 0 : {3,5}, shelf 1 : {1,4}
+            int[,] shelfCopies =
+            {
+                {3, 5 },
+                {1, 4 }
+            };
+
+            Console.WriteLine(shelfCopies[1,0]); // 1
+
             #endregion
         }
     }
