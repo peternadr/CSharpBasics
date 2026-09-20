f94778aaddc055494dcb18d679d1d34b1556c9c9
peter nader <peternader6565@outlook.com>
2026-09-20 14:49:15 +0300

    Question 01

---
+++ Assignment 07/Assignment 07/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 01
+            /*Add a private string password = "secret"; field to a Book class. Try to print it from Main
+(outside the class). What happens, and why?*/
+
+            //Book book = new Book();
+
+            //Console.WriteLine(book.password); // error
+            //we can not access password only in Book class
+
+            #endregion
+        }
+    }
+}
