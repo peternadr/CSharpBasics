75c38205887e5b4b00303105174e8df079ad525e
peter nader <peternader6565@outlook.com>
2026-09-20 14:54:27 +0300

    Question 02

---
+++ Assignment 07/Assignment 07/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_07;
+
+internal class Book
+{
+    private string password = "secret";
+    internal int copiesInStock = 5;
+}
--- Assignment 07/Assignment 07/Program.cs
+++ Assignment 07/Assignment 07/Program.cs
@@ -8,10 +8,19 @@
             /*Add a private string password = "secret"; field to a Book class. Try to print it from Main
 (outside the class). What happens, and why?*/
 
-            //Book book = new Book();
+            Book book = new Book();
 
             //Console.WriteLine(book.password); // error
             //we can not access password only in Book class
+
+            #endregion
+
+            #region Question 02
+            /*Add an internal int copiesInStock = 5; field to Book. Print it from Main. Does it compile?
+Why?*/
+            
+            Console.WriteLine(book.copiesInStock);
+            // Compile, We can access internal feild in the same project only
 
             #endregion
         }
