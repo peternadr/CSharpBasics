7a18aaae4af63b4efc1380a4b6dbe114086932e2
peter nader <peternader6565@outlook.com>
2026-09-20 14:57:11 +0300

    Question 03

--- Assignment 07/Assignment 07/Book.cs
+++ Assignment 07/Assignment 07/Book.cs
@@ -8,4 +8,6 @@ internal class Book
 {
     private string password = "secret";
     internal int copiesInStock = 5;
+
+    public string Title;
 }
--- Assignment 07/Assignment 07/Program.cs
+++ Assignment 07/Assignment 07/Program.cs
@@ -18,10 +18,17 @@
             #region Question 02
             /*Add an internal int copiesInStock = 5; field to Book. Print it from Main. Does it compile?
 Why?*/
-            
-            Console.WriteLine(book.copiesInStock);
+
+            //Console.WriteLine(book.copiesInStock);
             // Compile, We can access internal feild in the same project only
 
+            #endregion
+
+            #region Question 03
+            /*Add a public string Title; field to Book. Set it and print it from Main.*/
+
+            book.Title = "Clean code";
+            Console.WriteLine(book.Title);
             #endregion
         }
     }
